cb200ff4d20a0d7f92e788fa8d314e818c79110e
Matt Harrison <maharrison@sasint.co.uk>
2020-09-10 14:44:56 +0100

    Question 10 updates

--- 10/asteroid.py
+++ 10/asteroid.py
@@ -1,13 +1,26 @@
+import numpy as np
 input = []
-with open("test1.txt", "r") as f:
+asteroid = []
+from os.path import dirname, join
+current_dir = dirname(__file__)
+file_path = join(current_dir, "./test1.txt")
+
+with open(file_path, "r") as f:
 	for i,line in enumerate(f):
 		input.append(list(line.strip()))
 
-for row in input:
-	for pixel in row:
-		if pixel == "#":
-			print("found one")
+for y,row in enumerate(input):
+	for x,column in enumerate(row):
+		if column == '#':
+			asteroid.append([x,y])
 
+for station in asteroid:
+	observation = []
+	for target in asteroid:
+		if target != station:
+			
+			print(target[1]-station[1]/target[0]-station[0])
 
-print(input)
-
+			
+print(asteroid)
+print(np.matrix(input))
