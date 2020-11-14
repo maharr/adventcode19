467f0b5bb0f3d6943dda3ebfd190bafc6ae5682b
Matt Harrison <matt.harrison91@gmail.com>
2020-11-14 13:37:30 +0000

    complete q10 part 1

--- 10/asteroid.py
+++ 10/asteroid.py
@@ -1,9 +1,11 @@
 import numpy as np
+from numpy import unravel_index
+import math
 input = []
 asteroid = []
 from os.path import dirname, join
 current_dir = dirname(__file__)
-file_path = join(current_dir, "./test1.txt")
+file_path = join(current_dir, "./input.txt")
 
 with open(file_path, "r") as f:
 	for i,line in enumerate(f):
@@ -14,13 +16,23 @@ for y,row in enumerate(input):
 		if column == '#':
 			asteroid.append([x,y])
 
+count = {}
 for station in asteroid:
 	observation = []
 	for target in asteroid:
 		if target != station:
-			
-			print(target[1]-station[1]/target[0]-station[0])
+			observation.append(math.atan2(target[1]-station[1],target[0]-station[0]))
+	observation = list(dict.fromkeys(observation))
+	input[station[1]][station[0]] = len(observation)
 
-			
-print(asteroid)
-print(np.matrix(input))
+print(input)
+
+max = [0,0,0]
+for y,row in enumerate(input):
+	for x,column in enumerate(row):
+		try:
+			if column > max[0]:
+				max = [column, x, y]
+				print(max)
+		except:
+			pass
